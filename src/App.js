ed4c0419cdb36a2a66e04b5f93449252392e773a
Ariel Spencer <arielspencer.tech@gmail.com>
2024-07-12 21:52:31 -0300

    Implementar rodapé

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Banner from './components/Banner';
 import About from './components/About';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Banner />
       <About />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <footer className='footer'>
+            <p>Ariel Spencer © 2024 Todos os direitos reservados.</p>
+        </footer>
+    )
+}
+
+export default Footer
